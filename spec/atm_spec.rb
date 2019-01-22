84c76e7b4a2a42a54e951d34c7ee69e0492306de
Noel Ryan <noelgerard.ryan@gmail.com>
2019-01-22 12:24:47 +0100

    refactoring cod in the atm.rb, added require 'date' into spec folder

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -1,12 +1,25 @@
 require './lib/atm.rb'
+require 'date'
 
 describe Atm do 
+    let (:account) {instance_double( 'Account')}
+    
+    before do
+        allow(account).to receive(:balance).and_return(100)
+        allow(account).to receive(:balance=)
+    end
+
+    it 'allow withdraw if account has enough balance.' do
+        expect_output = { status: true, message: 'success', date: Date.today, amount: 45 }
+        expect(subject.withdraw(45, account)).to eq expect_output
+    end
+
     it 'has 1000$ on initialize' do
         expect(subject.funds).to eq 1000
     end
 
     it 'funds are reduced at withdraw' do
-        subject.withdraw(50)
+        subject.withdraw(50, account)
         expect(subject.funds).to eq 950
     end
 end
